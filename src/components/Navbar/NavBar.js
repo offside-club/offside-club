f48a6b036851efe6c55e419cc0b8907b6e6eff87
UT-03 <f20200458@pilani.bits-pilani.ac.in>
2023-02-26 23:19:41 +0530

    fix: add nav items

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import LogoNavbar from '../../assets/images/offside_logo_main.svg';
+import AppStoreLogo from '../../assets/images/app_store_logo.svg';
+import PlayStoreLogo from '../../assets/images/play_store_logo.svg';
+import constants from '../../util/constants';
 
 const NavBar = () => {
     return (
@@ -15,6 +22,24 @@ const NavBar = () => {
                             className="d-inline-block align-top navbar-logo"
                         />
                     </Navbar.Brand>
+                    <Nav className="ms-auto">
+                        <Nav.Item>
+                            <Nav.Link href={constants.appStoreURL}>
+                                <Image src={AppStoreLogo} className="navbar-store-logo navbar-app-store-logo" />
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href={constants.playStoreURL}>
+                                <Image src={PlayStoreLogo} className="navbar-store-logo navbar-play-store-logo" />
+                            </Nav.Link>
+                        </Nav.Item>
+                        {/* <a href={constants.appStoreURL}>
+                                <Image src={AppStoreLogo} className="navbar-store-logo navbar-app-store-logo" />
+                            </a>
+                            <a href={constants.playStoreURL}>
+                                <Image src={PlayStoreLogo} className="navbar-store-logo navbar-play-store-logo" />
+                            </a> */}
+                    </Nav>
                 </Container>
             </Navbar>
         </React.Fragment>
